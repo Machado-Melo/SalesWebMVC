e16564930402032d91526bf5eb0a9621c92f56fb
Guilherme Melo <guilube23@gmail.com>
2021-12-23 11:13:46 -0400

    This is a web application MVC, .NET CORE that mysql as it database

---
+++ SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public string Message { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
